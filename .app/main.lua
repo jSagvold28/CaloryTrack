ec6a9fb1a5c4a6bc147f948ca6339ded7fc45168
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-30 21:01:16 -0600

    sv commit

--- .app/main.lua
+++ .app/main.lua
@@ -341,6 +341,12 @@ while true do
     elseif input == "week" then
         planMealWeek()
 
+    elseif input == "signup" then
+        signup()
+
+    elseif input == "login" then
+        login()
+
     end
 end
 
@@ -376,3 +382,112 @@ local function resetWaterCount()
     end
 end
 resetWaterCount()
+
+
+print("Sign up was completed!")
+
+
+local function fetchUserDetails()
+
+    print(userName)
+    print(displayName)
+
+    print("Wondering where the other details are? Please check the userDetails.txt file.")
+
+end
+fetchUserDetails()
+
+local function deleteUserLog()
+
+    local input = io.read()
+
+    if input == "!del" then
+        local files = io.popen("ls *.txt") 
+        local fileList = files:read("*all") 
+
+        files:close()
+        
+        for file in string.gmatch(fileList, "%S+") do 
+
+            os.remove(file) -- Use the os.remove() function to delete each file
+
+        end
+
+        print("User data files, have been kicked off the server.")
+    end
+end
+deleteUserLog()
+
+-- Account functions
+
+local function signup()
+
+    print("Name: ")
+    local userName = io.read()
+
+    print("User Name:")
+    local displayName = io.read()
+
+    print("Password:")
+    local password = io.read()
+
+    print("Age: ")
+    local userAge = io.read()
+
+    print("Height: ")
+    local userHeight = io.read()
+
+    print("Weight: ")
+    local userWeight = io.read()
+
+    print("")
+    print("Non-essental information, you can skip")
+    print("")
+
+
+    print("Daily liquid intake: ")
+    local dailyLiquidIntake = io.read()
+
+    print("Daily water intake: ")
+    local dailyWaterIntake = io.read()
+
+    print("Total active minuites (per day): ")
+    local activeMoveMinutesPD = io.read()
+
+    print("Total steps per day: ")
+    local totalStepsPD = io.read()
+
+    local file = io.open("userDetails.txt", "a")
+
+    local userData = userName .. displayName .. userAge .. userWeight .. userWeight
+    local displayData = userName .. displayName .. userAge
+    local privateData = password .. userWeight .. userAge
+    
+    
+    file:write(displayData .. "\n")
+    file:close()
+
+end
+signup()
+
+
+local function login()
+
+    print("Enter your name: ")
+    local userName = io.read()
+
+    print("Password")
+    local password = io.read()
+
+    local file = io.open("users.txt", "r")
+    for line in file:lines() do
+        local data = string.split(line, ",")
+        if data[1] == name and data[2] == age then
+            file:close()
+
+            return true
+    end
+    file:close()
+    return false
+end
+login()
